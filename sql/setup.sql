ba5253e47e6ea3b2481636b842667fd6ab2f2666
austinbhan <austinbhan@outlook.com>
2022-08-25 15:49:32 -0700

    users_tasks SQL

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,8 +1,8 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS tasks CASCADE;
-
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS users_tasks CASCADE;
 
 CREATE TABLE users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -13,4 +13,12 @@ CREATE TABLE users (
 CREATE TABLE tasks (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     description VARCHAR
-)
+);
+
+CREATE TABLE users_tasks (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id BIGINT,
+    task_id BIGINT,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (task_id) REFERENCES tasks(id)
+);
